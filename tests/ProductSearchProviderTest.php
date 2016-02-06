c3fe7791c52fa96f82fe7a539d0fd1ca51d5d13d
djfm <fm.de.jouvencel@gmail.com>
2016-02-06 19:04:34 +0100

    // combined to test methods using dataprovider

--- tests/ProductSearchProviderTest.php
+++ tests/ProductSearchProviderTest.php
@@ -33,43 +33,19 @@ class ProductSearchProviderTest extends PHPUnit_Framework_TestCase
         );
     }
 
-    public function dataProvider_for_test_search_products_in_category_correct_count()
+    public function dataProvider_for_test_search_products_in_category_has_correct_total_count()
     {
         return [
-            [2, 7]
-        ];
-    }
-
-    public function dataProvider_for_test_search_products_in_category_with_attribute_correct_count()
-    {
-        return [
+            [2, null, 7],
             [2, '@-Color-Blue', 2],
             [2, '@-Color-Blue-Green', 3]
         ];
     }
 
     /**
-     * @dataProvider dataProvider_for_test_search_products_in_category_correct_count
+     * @dataProvider dataProvider_for_test_search_products_in_category_has_correct_total_count
      */
-    public function test_search_products_in_category_correct_count($id_category, $expected_count)
-    {
-        $query = (new ProductSearchQuery)
-            ->setQueryType('category')
-            ->setIdCategory($id_category)
-        ;
-
-        $result = $this
-            ->getProductSearchProvider($query)
-            ->runQuery($this->getProductSearchContext(), $query)
-        ;
-
-        $this->assertEquals($expected_count, $result->getTotalProductsCount());
-    }
-
-    /**
-     * @dataProvider dataProvider_for_test_search_products_in_category_with_attribute_correct_count
-     */
-    public function test_search_products_in_category_with_facets_count(
+    public function test_search_products_in_category_has_correct_total_count(
         $id_category,
         $encodedFacets,
         $expected_count
